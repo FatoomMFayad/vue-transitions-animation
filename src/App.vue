46c953da42ac372b4f38579012d0d4c09ec32606
FatoomMFayad <fatoomfayyad@gmail.com>
2020-09-17 10:58:48 +0300

    adding css classes for transitions

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
                 <hr>
                 <button class="btn btn-primary" @click="show = !show ">Show Alert</button>
                 <br><br>
-                <transition>
+                <transition name="fade">
                     <div class="alert alert-info" v-if="show">This is an alert</div>
                 </transition>
             </div>
@@ -26,5 +26,19 @@
 </script>
 
 <style>
+    .fade-enter {
 
+    }
+
+    .fade-enter-active {
+
+    }
+
+    .fade-leave {
+
+    }
+
+    .fade-leave-active {
+        
+    }
 </style>
